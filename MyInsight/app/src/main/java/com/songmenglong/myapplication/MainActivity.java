d28bd8112d2881b35cbed02f58d02d3ece49dc9f
MagnumSong <983174628@qq.com>
2018-02-11 19:59:02 +0800

    二十四桥明月夜

--- MyInsight/app/src/main/java/com/songmenglong/myapplication/MainActivity.java
+++ MyInsight/app/src/main/java/com/songmenglong/myapplication/MainActivity.java
@@ -22,50 +22,59 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;  //声明一个ListView对象
-    private List<Info> mlistInfo = new ArrayList<Info>();  //声明一个list，动态存储要显示的信息
+    //用于添加每一个选项卡的id
+    private String[] tags = {"A_tag", "B_tag", "C_tag", "D_tag"};
+    //所添加选项卡的文本信息
+    private String[] titles = {"新闻", "图片", "视频", "收藏"};
+    //所添加选项卡的图片信息
+    private int[] images = {R.drawable.tihu, R.drawable.tihu, R.drawable.tihu, R.drawable.tihu};
+    //用于跳转至不同的Activity
+    private Intent[] intents = new Intent[4];
+
+    //ListView listView;  //声明一个ListView对象
+    //private List<Info> mlistInfo = new ArrayList<Info>();  //声明一个list，动态存储要显示的信息
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView)this.findViewById(R.id.listView);    //将listView与布局对象关联
-
-        setInfo();  //给信息赋值函数，用来测试
-
-        Log.i("滚滚长江东逝水", mlistInfo.toString());
-
-        listView.setAdapter(new ListViewAdapter(mlistInfo));
-
-        //处理Item的点击事件
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Info getObject = mlistInfo.get(position);   //通过position获取所点击的对象
-                int infoId = getObject.getId(); //获取信息id
-                String infoTitle = getObject.getTitle();    //获取信息标题
-                String infoDetails = getObject.getDetails();    //获取信息详情
-
-                //Toast显示测试
-                Toast.makeText(MainActivity.this, "信息ID:"+infoId,Toast.LENGTH_SHORT).show();
-
-                // 进入到下一页面
-                // 给bnt1添加点击响应事件
-                Intent intent =new Intent(MainActivity.this, PlaySound.class);
-                //启动
-                startActivity(intent);
-            }
-        });
-
-        //长按菜单显示
-        listView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
-            public void onCreateContextMenu(ContextMenu conMenu, View view , ContextMenu.ContextMenuInfo info) {
-                conMenu.setHeaderTitle("菜单");
-                conMenu.add(0, 0, 0, "条目一");
-                conMenu.add(0, 1, 1, "条目二");
-                conMenu.add(0, 2, 2, "条目三");
-            }
-        });
+//        listView = (ListView)this.findViewById(R.id.listView);    //将listView与布局对象关联
+//
+//        setInfo();  //给信息赋值函数，用来测试
+//
+//        Log.i("滚滚长江东逝水", mlistInfo.toString());
+//
+//        listView.setAdapter(new ListViewAdapter(mlistInfo));
+//
+//        //处理Item的点击事件
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Info getObject = mlistInfo.get(position);   //通过position获取所点击的对象
+//                int infoId = getObject.getId(); //获取信息id
+//                String infoTitle = getObject.getTitle();    //获取信息标题
+//                String infoDetails = getObject.getDetails();    //获取信息详情
+//
+//                //Toast显示测试
+//                Toast.makeText(MainActivity.this, "信息ID:"+infoId,Toast.LENGTH_SHORT).show();
+//
+//                // 进入到下一页面
+//                // 给bnt1添加点击响应事件
+//                Intent intent =new Intent(MainActivity.this, PlaySound.class);
+//                //启动
+//                startActivity(intent);
+//            }
+//        });
+//
+//        //长按菜单显示
+//        listView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+//            public void onCreateContextMenu(ContextMenu conMenu, View view , ContextMenu.ContextMenuInfo info) {
+//                conMenu.setHeaderTitle("菜单");
+//                conMenu.add(0, 0, 0, "条目一");
+//                conMenu.add(0, 1, 1, "条目二");
+//                conMenu.add(0, 2, 2, "条目三");
+//            }
+//        });
     }
 
 
@@ -141,21 +150,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void setInfo() {
-        //mlistInfo.clear();
-        int i = 0;
-        while(i < 20) {
-            Info information = new Info();
-            information.setId(1000+i);
-            information.setTitle("标题"+i);
-            information.setDetails("详细信息"+i);
-            information.setAvatar(R.drawable.tihu); //图片没有
-
-            // 将新的info对象加入到信息列表中
-            mlistInfo.add(information);
-            i++;
-        }
-    }
+//    public void setInfo() {
+//        //mlistInfo.clear();
+//        int i = 0;
+//        while(i < 20) {
+//            Info information = new Info();
+//            information.setId(1000+i);
+//            information.setTitle("标题"+i);
+//            information.setDetails("详细信息"+i);
+//            information.setAvatar(R.drawable.tihu); //图片没有
+//
+//            // 将新的info对象加入到信息列表中
+//            mlistInfo.add(information);
+//            i++;
+//        }
+//    }
 
 }
 
