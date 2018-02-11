2bfd950c62897cb5f09287b0c544d147354334c3
MagnumSong <983174628@qq.com>
2018-02-11 20:21:29 +0800

    玉人何处教吹箫

--- MyInsight/app/src/main/java/com/songmenglong/myapplication/MainActivity.java
+++ MyInsight/app/src/main/java/com/songmenglong/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.songmenglong.myapplication;
 
+import android.app.LocalActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,8 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -75,8 +79,51 @@ public class MainActivity extends AppCompatActivity {
 //                conMenu.add(0, 2, 2, "条目三");
 //            }
 //        });
+
+
+
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
+        TabWidget tabWidget = (TabWidget) findViewById(android.R.id.tabs);
+
+        //tabWidget.setDividerDrawable(null); //设置tabWeight没有竖线分割
+        tabWidget.setBackgroundColor(Color.WHITE);
+        //初始化activity管理者
+        LocalActivityManager manager = new LocalActivityManager(MainActivity.this, false);
+        //通过管理者保存当前页面状态
+        manager.dispatchCreate(savedInstanceState);
+        //将管理者类对象添加至TabHost
+        tabHost.setup(manager);
+
+        init_intent();
+
+        for (int i = 0; i < intents.length; i++) {
+            //加载底部导航栏布局
+            LayoutInflater inflater = this.getLayoutInflater();
+            View view = inflater.inflate(R.layout.tab, null);
+            TextView textView = (TextView) view.findViewById(R.id.tv_item);
+            ImageView imageView = (ImageView) view.findViewById(R.id.image);
+            textView.setText(titles[i]);
+            imageView.setImageResource(images[i]);
+            //创建选项卡
+            TabHost.TabSpec spec = tabHost.newTabSpec(tags[i]);
+            spec.setIndicator(view);
+            //设置每个页面的内容
+            spec.setContent(intents[i]);
+            //将创建的选项卡添加至tabHost上
+            tabHost.addTab(spec);
+        }
+
     }
 
+
+    //每个页面放置的Activity
+    public void init_intent() {
+        intents[0] = new Intent(this, NewsActivity.class);
+        intents[1] = new Intent(this, PhotoActivity.class);
+        intents[2] = new Intent(this, VideoActivity.class);
+        intents[3] = new Intent(this, CollectionActivity.class);
+    }
 
     // 长按菜单处理函数
     public boolean onContextItemSelected(MenuItem aItem) {
