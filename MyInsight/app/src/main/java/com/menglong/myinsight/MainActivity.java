8c42021ae4a29481415f00860f1813679e1d516e
MagnumSong <983174628@qq.com>
2018-05-23 15:02:27 +0800

    岁岁年年花相似

---
+++ MyInsight/app/src/main/java/com/menglong/myinsight/MainActivity.java
@@ -0,0 +1,22 @@
+package com.menglong.myinsight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/*
+*
+* 准备写一个demo 仿照iOS demo的样子
+*
+* */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //
+        setContentView(R.layout.activity_main);
+    }
+}
