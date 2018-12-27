8d4daffec50f12cdd8550efcb629b6e4db9ad5d4
MenglongSong <983174628@qq.com>
2018-12-27 23:40:38 +0800

    tomcat 试试

---
+++ FilesClippers/app/src/main/java/com/songmenglong/filesclippers/BLEScanActivity.java
@@ -0,0 +1,15 @@
+package com.songmenglong.filesclippers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BLEScanActivity extends AppCompatActivity {
+
+
+    // 扫描蓝牙的页面
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blescan);
+    }
+}
